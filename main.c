f99ba10901a3dc9db9b0421daa50128b5b380b2f
Kiel Oleson <kiel@jusquici.org>
2023-12-09 00:02:15 -0800

    refactor getting capture interface before it gets out of hand

--- main.c
+++ main.c
@@ -4,13 +4,36 @@
 
 // http://yuba.stanford.edu/~casado/pcap/section1.html
 
+pcap_if_t* get_interface(int argc, char** argv, char* errbuf);
+
 int main(int argc, char** argv) {
-  printf("argc: %d\n", argc);
-  char* dev; /* name of device to use */
-  char* net; /* dot notation of the network address */
-  char* mask; /* dot notation of the network mask */
+  
+  
+  /*
+  we actually don't care about the network address or mask
+  of the interface we capture on since we assume only the
+  PVS and ESS are on the interface.
+  kmo 8 dec 2023 23h46
+  */
+  // char* net; /* dot notation of the network address */
+  // char* mask; /* dot notation of the network mask */
+
   int retval; /* return code */
   char errbuf[PCAP_ERRBUF_SIZE];
+
+  pcap_if_t* selected_interface = get_interface(argc, argv, errbuf);
+
+}
+
+pcap_if_t* get_interface(int argc, char** argv, char* errbuf) {
+  printf("argc: %d\n", argc);
+  if (argc < 2) {
+    printf("no arguments - using first available en or eth interface\n");
+  }
+  if (argc > 2) {
+    printf("too many arguments, confused, bailing out\n");
+    exit(1);
+  }
 
   pcap_if_t *all_devs, *selected_device;
 
@@ -24,7 +47,9 @@ int main(int argc, char** argv) {
   int devnum = 0;
   for (selected_device = all_devs; selected_device != NULL; selected_device = selected_device->next) {
     printf("device %d: %s - %s\n", devnum, selected_device->name, selected_device->description);
+    // TODO:  compare selected device name against argument or "en"/"eth"
     devnum++;
   }
 
+  return NULL;
 }
