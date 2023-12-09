5973d54c0ba52ff8bc6731b63a3bc919d908b3b5
Kiel Oleson <kiel@jusquici.org>
2023-12-08 22:28:04 -0800

    look at available devices with `pcap_findalldevs`

--- main.c
+++ main.c
@@ -1,23 +1,30 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <pcap.h>
 
 // http://yuba.stanford.edu/~casado/pcap/section1.html
 
 int main(int argc, char** argv) {
+  printf("argc: %d\n", argc);
   char* dev; /* name of device to use */
   char* net; /* dot notation of the network address */
   char* mask; /* dot notation of the network mask */
   int retval; /* return code */
   char errbuf[PCAP_ERRBUF_SIZE];
 
-  /*
-  lookupdev, as you might guess, is deprecated now that computers typically
-  have more than one network port. 
-  kmo 8 dec 2023 20h29
-  */
+  pcap_if_t *all_devs, *selected_device;
 
-  dev = pcap_lookupdev(errbuf);
+  int findalldevs_return = pcap_findalldevs(&all_devs, errbuf);
 
-  printf("DEV: %s\n", dev);
-  printf("hello!\n");
+  if (findalldevs_return != 0) {
+    printf("error finding devices: %s\n", errbuf);
+    exit(1);
+  }
+
+  int devnum = 0;
+  for (selected_device = all_devs; selected_device != NULL; selected_device = selected_device->next) {
+    printf("device %d: %s - %s\n", devnum, selected_device->name, selected_device->description);
+    devnum++;
+  }
+
 }
