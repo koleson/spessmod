c653becd2892d2a09632b66899d2c8a54683f49d
Kiel Oleson <kiel@jusuqici.org>
2023-12-09 22:08:33 -0800

    print to printf

--- main.c
+++ main.c
@@ -173,7 +173,7 @@ int main(int argc, char **argv)
   LOG_INFO("hex dump follows:")
   printf("\n\n");
   for (int byte = 0; byte < data_length; byte++) {
-    print("%02x ", data[byte]);
+    printf("%02x ", data[byte]);
   }
   printf("\n\n");
 
