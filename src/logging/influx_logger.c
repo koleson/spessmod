dc2527bde82b414f0243f38057db9dcded24e317
Kiel Oleson <kiel+github@kmo.email>
2024-01-22 15:35:03 -0800

    first stab at logging register/value pairs to console as line protocol fragment

--- src/logging/influx_logger.c
+++ src/logging/influx_logger.c
@@ -57,16 +57,21 @@ void influx_log_raw(uint8_t unit, uint16_t register_num, uint16_t value) {
 
 void influx_log_response(struct Modbus_Response* response) {
   LOG_INFO("==== start influx_log_response");
-  LOG_WARN("would log response struct %p to influx here but it's not implemented.", response);
-  LOG_INFO("packet function code should be 3 - is %d", response->data->function_code);
   LOG_INFO("unit number: %d, base register: %d", response->data->unit, response->context->base_register);
   char* data_prefix = "modbus,unit=%d ";
 
-  // we know the max length of register number and uint16_t, we could pre-allocate
-  // a string buffer entirely ahead of time.
+  // we know the max length of register number and uint16_t (both max 65535, so 5 digits),
+  // we could pre-allocate a string buffer entirely ahead of time.
+  // fmt regXXXXX=XXXXXi, is 16 bytes
   // kmo 22 jan 2024 14h26
+
+  
+
   unsigned int register_count = response->data->byte_count / 2;    // 16 bits = 1 word = 2 bytes
   LOG_INFO("influx_log_response:  register_count = %d", register_count);
+  
+  char register_values_string[16 * register_count];
+  char* register_values_string_cursor = register_values_string;
   
   unsigned int cursor = 0;
   while (cursor < register_count) {
@@ -75,8 +80,18 @@ void influx_log_response(struct Modbus_Response* response) {
 
     uint16_t register_value = response->data->register_data[cursor];
     LOG_INFO("influx_log_response: register value %d", register_value);
-    
+    snprintf(register_values_string_cursor, 15, "reg%d=%di", register_number, register_value);
+    register_values_string_cursor += 15;
+
+    // if not last register/value pair, add a comma
+    if (cursor < register_count - 1) {
+      sprintf(register_values_string_cursor, ",");
+      register_values_string_cursor++;
+    }
     cursor++;
+
+    register_values_string_cursor[0] = '\0';    // will get overwritten by next iteration if necessary
+    LOG_DEBUG("current register/values string: %s", register_values_string);
   }
 
   LOG_INFO("==== end influx_log_response");
