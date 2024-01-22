f1d1096e1be3aaeded7844462113c0d29a265118
Kiel Oleson <kiel+github@kmo.email>
2024-01-22 12:24:37 -0800

    add structs preparing for external packet processing

--- src/main/include/packet_processing.h
+++ src/main/include/packet_processing.h
@@ -7,6 +7,28 @@
 
 #include <pcap/pcap.h>
 
+struct Modbus_Response_Context {
+  uint64_t pcap_packet_number;
+  uint16_t base_register;
+};
+
+struct Modbus_Response_Data {
+  uint16_t transaction;
+  uint16_t protocol;
+  uint16_t length;
+  uint8_t unit;
+  uint8_t function_code;
+  uint8_t byte_count;
+  uint16_t* register_data;
+};
+
+struct Modbus_Response {
+  struct Modbus_Response_Context* context;
+  struct Modbus_Response_Data* data;
+};
+
+// TODO:  method to free Modbus_Response (really, register_data)
+
 void print_struct_sizes();
 int add_filter(pcap_t* pcap);
 void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
